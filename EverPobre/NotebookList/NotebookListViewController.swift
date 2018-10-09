d0a001a255d64203324a019efd3f16b5ea9d5044
Eduardo Jordan <eduardojordan@hotmail.com>
2018-10-09 14:10:05 +0200

    Creation StoryB.+ Stack +tableView

---
+++ EverPobre/NotebookList/NotebookListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotebookListViewController.swift
+//  EverPobre
+//
+//  Created by Eduardo on 9/10/18.
+//  Copyright © 2018 Eduardo Jordan Muñoz. All rights reserved.
+//
+
+import UIKit
+
+class NotebookListViewController: UIViewController{
+    
+}
